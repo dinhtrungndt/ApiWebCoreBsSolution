38db99b685f221d8ebaa256607837abf39e0b247
kho quản lý <nguyendinhtrung.it@gmail.com>
2024-06-28 23:04:34 +0700

    upload get

---
+++ ApiWebCoreBs/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiWebCoreBs.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public HttpResponseMessage GetAll()
+        {
+            string[] producs = { "TV", "Washing Machine", "Fridge", "Microwave" };
+            return Request.CreateResponse(HttpStatusCode.OK, producs);
+        }
+
+        public HttpResponseMessage GetProduct(int id)
+        {
+            string[] producs = { "TV", "Washing Machine", "Fridge", "Microwave" };
+            if(id >= 0 && id <= 3)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, producs[id]);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid ID");
+            }
+        }
+
+        public IHttpActionResult PostMessage(String name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                return Ok("Hello " + name);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}

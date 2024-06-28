e3d70b6c4ab38cb95204540bfcc2e46a613c094b
kho quản lý <nguyendinhtrung.it@gmail.com>
2024-06-28 23:29:42 +0700

    upload model

--- ApiWebCoreBs/Controllers/ProductController.cs
+++ ApiWebCoreBs/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiWebCoreBs.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,18 +10,26 @@ namespace ApiWebCoreBs.Controllers
 {
     public class ProductController : ApiController
     {
+        List<Product> products = new List<Product>()
+        {
+            new Product {ProductID = 1, Name = "TV", Price = 60000},
+            new Product {ProductID = 2, Name = "Washing Machine", Price = 40000},
+            new Product {ProductID = 3, Name = "Fridge", Price = 100000},
+
+        };
+
         public HttpResponseMessage GetAll()
         {
-            string[] producs = { "TV", "Washing Machine", "Fridge", "Microwave" };
-            return Request.CreateResponse(HttpStatusCode.OK, producs);
+            // string[] products = { "TV", "Washing Machine", "Fridge", "Microwave" };
+            return Request.CreateResponse(HttpStatusCode.OK, products);
         }
 
         public HttpResponseMessage GetProduct(int id)
         {
-            string[] producs = { "TV", "Washing Machine", "Fridge", "Microwave" };
-            if(id >= 0 && id <= 3)
+            // string[] products = { "TV", "Washing Machine", "Fridge", "Microwave" };
+            if (id >= 0 && id <= 3)
             {
-                return Request.CreateResponse(HttpStatusCode.OK, producs[id]);
+                return Request.CreateResponse(HttpStatusCode.OK, products[id]);
             }
             else
             {
---
+++ ApiWebCoreBs/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiWebCoreBs.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
